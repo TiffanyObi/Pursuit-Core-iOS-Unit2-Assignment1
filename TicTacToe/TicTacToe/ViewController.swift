f1d2289b4852309298aae828979e01903cd660f5
Tiffany Obi <tiffanyobi123@gmail.com>
2019-11-05 17:24:49 -0500

    added buttons and trying to change turns

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var gameButtons: [GameButton]!
+    
+    @IBOutlet weak var playersTurn: UILabel!
+    
+    
+    
+    
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    playersTurn.text = "Player One's Turn"
+        
   }
 
+    @IBAction func gameButtonPressed(_ gameButton: GameButton) {
+        print("row\(gameButton.row) at column \(gameButton.col) was selected")
+        playersTurns()
+    }
+    
+    func playersTurns () {
+        var playerTwosTurn = true
+        
+        playerTwosTurn.toggle()
+        
+        if playerTwosTurn == true {
+            playersTurn.text = "Player Two's Turn"
+        } else if playerTwosTurn == false { playersTurn.text = "Player One's Turn"
+    }
 
 }
 
+}
