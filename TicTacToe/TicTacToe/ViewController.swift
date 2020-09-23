ac8af6797221ac286d14557e362cca7c1bc6ae03
Tiffany Obi <tiffanyobi123@gmail.com>
2020-09-23 14:38:44 -0400

    completed all requirements for Tic-tac-toe project

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -10,22 +10,67 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
-    var playerGo = 1
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
+    @IBOutlet weak var button7: UIButton!
+    @IBOutlet weak var button8: UIButton!
+    @IBOutlet weak var button9: UIButton!
     
     
     @IBOutlet weak var playersTurn: UILabel!
     
+    @IBOutlet weak var player1ScoreLabel: UILabel!
     
+    @IBOutlet weak var player2ScoreLabel: UILabel!
+   
     
+      var playerGo = 1
+      
+      var xscore = 0
+      var oscore = 0
+    
+     var allButtons = [UIButton]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+   
+  playerGo = 1
         
-        playersTurn.text = "Player One's Turn"
         
     }
+    
+    func disableAllButtons() {
+        let buttons = [button1,button2,button3,button4,button5,button6,button7,button8,button9]
+        
+        for button in buttons {
+            button?.isEnabled = false
+        }
+        
+        xWins = []
+        oWins = []
+        
+        playerGo = 1
+        
+    }
+    
+    func enableAllButtons() {
+        
+           let buttons = [button1,button2,button3,button4,button5,button6,button7,button8,button9]
+           
+           for button in buttons {
+            
+            button?.setBackgroundImage(nil, for: .normal)
+            
+               button?.isEnabled = true
+           }
+        xWins = []
+        oWins = []
+        playerGo = 1
+       }
     
     @IBAction func switchPlayersTurn(_ sender: UIButton) {
         
@@ -33,47 +78,61 @@ class ViewController: UIViewController {
         print(playerGo)
         printXorO(sender:sender)
         appendTag(sender: sender)
-        displayWinner()
+        displayWinner(sender: sender)
         
     }
     
     
+    @IBAction func newGameButton(_ sender: UIButton) {
+         playersTurn.text = "Begin !"
+         enableAllButtons()
+        
+    }
+    
+    
+    @IBAction func resetGameButton(_ sender: UIButton) {
+        playersTurn.text = "Begin !"
+        enableAllButtons()
+        xscore = 0
+        player1ScoreLabel.text = "Player 1 = \(xscore)"
+        oscore = 0
+        player2ScoreLabel.text = "Player 2 = \(oscore)"
+    }
     
     func playersChangeTurns (){
         if playerGo % 2 == 0 {
             playersTurn.text = "Player Two's Turn"
-            playerGo += 1} else if playerGo % 2 == 1 {
+            
+        }
+        if playerGo % 2 == 1 {
             playersTurn.text = "Player One's Turn"
-            playerGo += 1
         }
     }
     
     
     func printXorO (sender:UIButton) {
         
-        if playersTurn.text == "Player One's Turn" {
+        if playerGo % 2 == 1 {
             sender.setBackgroundImage(UIImage(named: "xmark"), for: UIControl.State.normal)
-            
-            
-            
-        } else if playersTurn.text == "Player Two's Turn" {
+            playersTurn.text = "Player Two's Turn"
+             playerGo += 1
+        } else if  playerGo % 2 == 0 {
             sender.setBackgroundImage(UIImage(named:
                 "circle"), for: UIControl.State.normal)
-            
-            
+            playersTurn.text = "Player One's Turn"
+             playerGo += 1
         }
     }
     
     
     func appendTag (sender:UIButton) {
-        if playersTurn.text == "Player Two's Turn" {
+        if playerGo % 2 == 1 {
             oWins.append(sender.tag)
             sender.isEnabled = false
             print(oWins)
-            
-            
-            
-        } else if playersTurn.text == "Player One's Turn" {
+        }
+        
+        if playerGo % 2 == 0{
             xWins.append(sender.tag)
             sender.isEnabled = false
             print(xWins)
@@ -83,13 +142,20 @@ class ViewController: UIViewController {
     
     
     
-    func displayWinner () {
+    func displayWinner (sender:UIButton) {
+          
         for group in winningCombos {
             
             if xWins.contains(group[0]) && xWins.contains(group[1]) && xWins.contains(group[2])  {
                 playersTurn.text = " X Wins!!! "
+                xscore += 1
+                player1ScoreLabel.text = "Player 1 = \(xscore)"
+                disableAllButtons()
             } else if oWins.contains(group[0]) && oWins.contains(group[1]) && oWins.contains(group[2]) {
                 playersTurn.text = " O Wins !!! "
+                oscore += 1
+                player2ScoreLabel.text = "Player 2 = \(oscore)"
+                disableAllButtons()
             }
         }
         
